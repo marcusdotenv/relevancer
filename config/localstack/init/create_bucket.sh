f01f3c8eb4652a4355559cfd255b5feb8813c713
Marcus Cavalcante <mgc.cavalcante28@gmail.com>
2024-09-22 16:40:17 -0300

    :bug: wait for localstack before try create bucket

--- config/localstack/init/create_bucket.sh
+++ config/localstack/init/create_bucket.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-echo "Criando novo bucket"
+wait_for_localstack() {
+    echo "Waiting localstack..."
+    while ! aws --endpoint-url=http://localstack:4566 s3api list-buckets > /dev/null 2>&1; do
+        echo "Aguardando LocalStack estar pronto..."
+        sleep 2
+    done
+    echo "LocalStack ready!!"
+}
+
+
+wait_for_localstack
+
+echo "starting new bucket trie-bucket"
 aws --endpoint-url=http://localstack:4566 s3api create-bucket --bucket trie-bucket
-echo "Adicionando default trie"
+
+echo "loading to trie-bucket file default.json"
 aws --endpoint-url=http://localstack:4566 s3 cp /etc/localstack/init/default.json s3://trie-bucket/
